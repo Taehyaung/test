6c768af706e54736f6da3faca5edac204f64059e
Taehyaung <57709467+Taehyaung@users.noreply.github.com>
2022-02-23 15:43:37 +0800

    Add files via upload

---
+++ kline.py
@@ -0,0 +1,13 @@
+import requests
+import json
+import numpy as np
+import pandas as pd
+
+site = "https://query1.finance.yahoo.com/v8/finance/chart/2330.TW?period1=0&period2=1549258857&interval=1d&events=history&=hP2rOschxO0"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get(site,headers=headers)
+data = json.loads(response.text)
+df = pd.DataFrame(data['chart']['result'][0]['indicators']['quote'][0], index=pd.to_datetime(np.array(data['chart']['result'][0]['timestamp'])*1000*1000*1000))
+
+print(df.head)
